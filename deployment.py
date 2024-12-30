99532a2b3fdcc4110feaf0defa578269285a916a
LimKunZer <110703948+LimKunZer@users.noreply.github.com>
2024-12-30 13:09:48 +0800

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -96,7 +96,7 @@ def predict_sentiment(message, model, tokenizer, max_seq_len):
   with torch.no_grad():
     inputs = torch.tensor(padded, dtype=torch.long)
     outputs = model(inputs)
-    probabilities = torch.softmax(outputs, dim=1).cpu().numpy()
+    probabilities = torch.softmax(outputs, dim = 1).cpu().numpy()
     predicted_label = class_names[np.argmax(probabilities)]
   return predicted_label
 
